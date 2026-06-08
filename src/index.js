17fd78fbd67426249f2dbb73ec3f8379b743c741
Erick Gonzalez <gempro30yt@gmail.com>
2026-06-08 17:22:30 -0300

    fix(controllers): corregir sintaxis en controlador de mobs y middleware duplicado

--- src/index.js
+++ src/index.js
@@ -5,25 +5,6 @@ console.log(" FRONTEND_URL en memoria del servidor:", process.env.FRONTEND_URL);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('¡La API de Minecraft está funcionando perfectamente! 🚀 probá ir a /api/items o /api/mobs');
-});
-
-app.get('/api/health', (req, res) => {
-  res.status(200).json({
-    status: "ok",
-    message: "API funcionando correctamente"
-  });
-});
-
-app.use("/api/items", itemRoutes)
-app.use("/api/mobs", mobRoutes)
-
 app.listen(PORT, () => {
   console.log(`🚀 Servidor backend encendido y escuchando en http://localhost:${PORT}`);
 });
-
-module.exports = app;
